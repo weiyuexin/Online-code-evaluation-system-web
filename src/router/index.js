aa0e50c49848a091b1b0572575287b6912a8ff9f
weiyuexin <3022422894@qq.com>
2023-02-12 18:08:56 +0800

    add vue-router ＆ element-ui

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Main from '../views/Main'
+import Login from '../views/Login'
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+        {
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/',
+            component: Main
+        }
+    ]
+})
---
+++ src/views/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>首页</h1>
+    <div class="block">
+      <span class="demonstration">默认</span>
+      <el-date-picker
+          v-model="value1"
+          type="date"
+          placeholder="选择日期">
+      </el-date-picker>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data() {
+    return {
+      pickerOptions: {
+        disabledDate(time) {
+          return time.getTime() > Date.now();
+        },
+      },
+      value1: '',
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
